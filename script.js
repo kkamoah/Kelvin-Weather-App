bbdadc3ada0f1b668261c9f477cea946a2c0f79b
Kelvin Amoah <86424487+kkamoah@users.noreply.github.com>
2024-08-31 10:08:59 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const apiKey = '9f826d7e534b3cfecc124df21a876596';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+const weatherIcon = document.getElementById('weather-icon');
+
+searchButton.addEventListener('click', () => {
+    const location = locationInput.value;
+    if (location) {
+        fetchWeather(location);
+    }
+});
+
+function fetchWeather(location) {
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+            const iconCode = data.weather[0].icon;
+            const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+            weatherIcon.src = iconUrl;
+            weatherIcon.alt = data.weather[0].description;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data: ', error);
+        });
+}
